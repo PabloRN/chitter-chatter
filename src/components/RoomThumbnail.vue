da1e49cba077c2f07b2af6bca9232c3553d2ce7b
Pablo Reyes Naranjo <pablo.naranjo@mcon-group.com>
2024-05-11 22:00:36 +0800

    improve users online

--- src/components/RoomThumbnail.vue
+++ src/components/RoomThumbnail.vue
@@ -5,22 +5,19 @@
           <v-img :src="room.picture" class="white--text align-end"
            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
             height="200px">
-            <v-card-title v-text="room.nombre"></v-card-title>
+            <v-card-title class="roomtitle" v-text="room.nombre"></v-card-title>
           </v-img>
 
           <v-card-actions>
             <v-spacer></v-spacer>
 
-            <v-btn icon>
+            <!-- <v-btn icon>
               <v-icon>mdi-heart</v-icon>
-            </v-btn>
+            </v-btn> -->
 
             <v-btn icon>
-              {{ usersOnline }}
+              {{ usersOnline || 0 }}
               <v-icon>mdi mdi-account-group</v-icon>
-            </v-btn>
-            <v-btn icon>
-              <v-icon>mdi-share-variant</v-icon>
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -60,6 +57,6 @@ export default {
   },
 };
 </script>
-<style scoped>
+<style >
 
 </style>
