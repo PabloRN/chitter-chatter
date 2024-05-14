749db3610b37c1ab5b49de55be0d6df14788cc6d
Pablo Reyes Naranjo <pablo.naranjo@mcon-group.com>
2024-05-14 18:57:57 +0800

    improve bubble dialog

--- src/components/DialogBubble.vue
+++ src/components/DialogBubble.vue
@@ -50,50 +50,64 @@ export default {
   },
 };
 </script>
-<style scoped>
+<style scoped lang="scss">
 /* @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,300italic,regular,italic,500,500italic,700,700italic); */
 @import url(https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap);
 .bubble {
+  background-color:#fff;
+  border:solid 2px #000;
+  border-radius:12px;
+  display:inline-block;
+  margin:.5em;
+  padding:.5em 1em;
   position: absolute;
   font-family: "Nanum Pen Script", cursive!important;
-  font-size: 1.1rem!important;
+  font-size: 1.3rem!important;
   line-height: 1.1!important;
   width: 200px;
+  height: auto;
   background: #fff;
   border-radius: 6px;
   color: #000;
-  top: -60px;
-  max-height: 92px;
+  bottom: 215px;
+  left: 15px;
   text-align: center;
   box-shadow: 1px 1px 3px #424242;
-  z-index: 1000
-}
-.bubble-current {
-  position: absolute;
-  font-family: "Nanum Pen Script", cursive!important;
-  font-size: 1.1rem!important;
-  line-height: 1.1!important;
-  width: 200px;
-  background: #fff;
-  border-radius: 6px;
-  color: #000;
-  top: -60px;
-  max-height: 92px;
-  text-align: center;
-  box-shadow: 1px 1px 3px #424242;
-  z-index: 999;
+  z-index: 1000;
+  &-current{
+    @extend .bubble;
+    z-index: 999;
+    bottom: 225px;
+  }
 }
 
 .bubble-bottom-left:before {
-  content: "";
-    width: 0px;
-    height: 0px;
-    position: absolute;
-    border-left: 15px solid #fff;
-    border-right: 5px solid transparent;
-    border-top: 12px solid #fff;
-    border-bottom: 30px solid transparent;
-    left: 42px;
-    bottom: -28px;
+  border:solid 12px transparent;
+  border-left:solid 12px #000;
+  border-top:solid 12px #000;
+  bottom:-24px;
+  content:"";
+  height:0;
+  left:24px;
+  position:absolute;
+  transform:skew(-15deg);
+  width:0;
+}
+
+.bubble-bottom-left:after {
+  border:solid 10px transparent;
+  border-left:solid 10px #fff;
+  border-top:solid 10px #fff;
+  bottom:-19px;
+  content:"";
+  height:0;
+  left:27px;
+  position:absolute;
+  transform:skew(-15deg);
+  width:0;
+}
+
+textarea:focus, input:focus{
+    outline: none;
 }
 </style>
